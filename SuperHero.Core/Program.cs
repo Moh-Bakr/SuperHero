b2c126f79564b0d9903bb15fd994dbf35aa2be2f
Mohamed Bakr <m.bakr@munjiz.sa>
2023-11-18 12:00:52 +0200

    feat: Implement Repository Pattern with Unit of Work
    Create repository pattern architecture with a unit of work for improved data access and management. Set up the database context and connection string.

--- SuperHero.Core/Program.cs
+++ SuperHero.Core/Program.cs
@@ -1,19 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region GenericRepositories
+
+builder.Services.AddScoped(typeof(ICrudRepository<>), typeof(CrudRepository<>));
+
+#endregion
+
+#region DbContext and Identity
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+   .AddEntityFrameworkStores<ApplicationDbContext>()
+   .AddDefaultTokenProviders();
+
+#endregion
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   app.UseSwagger();
+   app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ SuperHero.DAL/Models/BaseModel/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperHero.Helper;
+
+public class BaseModel
+{
+   public DateTime? CreatedDate { get; set; }
+   public DateTime? ModifiedDate { get; set; }
+
+   protected BaseModel()
+   {
+      CreatedDate = DateTime.UtcNow;
+      ModifiedDate = DateTime.UtcNow;
+   }
+
+   public int Id { get; set; }
+}
